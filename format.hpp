1303ec76012f8fb40a7804fd540b8a1367094ce1
iMaximus <61899298+iiiMaximus@users.noreply.github.com>
2021-08-08 18:59:46 +0800

    Create format.hpp

---
+++ format.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+class format {
+public:
+	char buffer[2048];
+	std::string value;
+
+	template<typename fmt, typename... args>
+	format(const fmt& _fmt, args&&... _args) {
+		snprintf(buffer, sizeof(buffer), _fmt, _args...);
+	}
+
+	friend std::ostream& operator << (std::ofstream& _os, const format& _fmt) {
+		_os << _fmt.buffer;
+
+		return _os;
+	}
+
+	operator const char* () const {
+		return this->buffer;
+	}
+
+	operator std::string() const {
+		return this->buffer;
+	}
+
+	operator format() const {
+		return *this;
+	}
+};
